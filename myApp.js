00a108f2a7fb1a35a6107729219f91d2adcaf560
Favour Max-Oti <maxotif@gmail.com>
2023-10-07 05:48:34 +0100

    changed response to html page

--- myApp.js
+++ myApp.js
@@ -2,8 +2,8 @@ let express = require('express');
 let app = express();
 
 app.get( '/', function ( req, res )
-{ 
-    res.send( 'Hello Express' );
+{
+    res.sendFile(`${__dirname}/views/index.html`);
 } );
 
 
