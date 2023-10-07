f5f8dfc5f0f3615143d8bd4dcd06001087ee1182
Favour Max-Oti <maxotif@gmail.com>
2023-10-07 05:54:26 +0100

    updated to serve static files from app field

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let app = express();
 
+app.use(express.static(`${__dirname}/`));
+
 app.get( '/', function ( req, res )
 {
     res.sendFile(`${__dirname}/views/index.html`);
